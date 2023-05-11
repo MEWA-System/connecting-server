83b90b38d8be4cb0d47ab78cdc84aaebd283b08d
Mik3l24 <michalczus@gmail.com>
2023-05-11 20:36:50 +0200

    Added a base FastAPI project

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from psycopg import connect, Cursor
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/quest")
+async def fetch():
+    pass
