9dad1e6b30a8a39d63d87608e3390e4b0b6cc060
Mik3l24 <michalczus@gmail.com>
2023-05-31 17:02:00 +0200

    Implemented reading data from Modbus TCP/IP according to a YAML file

--- readings/data_classes.py
+++ readings/data_classes.py
@@ -5,6 +5,7 @@ import yaml
 class Meter(yaml.YAMLObject):
     yaml_loader = yaml.SafeLoader
     yaml_tag = u"meter"
+    client = None
 
     class Identification(yaml.YAMLObject):
         yaml_loader = yaml.SafeLoader
@@ -16,16 +17,26 @@ class Meter(yaml.YAMLObject):
             self.ip_address = ip_address
             self.tcp_socket = tcp_socket
 
-    def __init__(self, id: Identification, registers: dict):
+    class RegisterType(yaml.YAMLObject):
+        yaml_loader = yaml.SafeLoader
+        yaml_tag = u"reg_type"
+
+        def __init__(self, byteorder, wordorder, length):
+            self.byteorder = byteorder
+            self.wordorder = wordorder
+            self.length = length
+
+    def __init__(self, id: Identification, registers: dict, register_types: dict[str, RegisterType]):
         self.id = id
         self.registers = registers
+        self.register_types = register_types
 
 
 class Register(yaml.YAMLObject):
     yaml_loader = yaml.SafeLoader
     yaml_tag = u"reg"
 
-    def __init__(self, register: int, type):
+    def __init__(self, register: int, type: str):
         self.register = register
         self.type = type
 
--- readings/modbus.py
+++ readings/modbus.py
@@ -1,58 +1,123 @@
-from pymodbus import client as mbc, payload as mbp
-import yaml
-from data_classes import Meter, Register
 from typing import Optional
 
+import yaml
+from pymodbus import client as mbc, payload as mbp
 
-global data
-electric: Optional[Meter] = None
+from data_classes import Meter, Register
 
-# klient dla każdego urządzenia
-
-
-async def init():
-    load_register_reference()
-    # stwórz klienta dla każdego urządzenia
+# Global variables
+meters: Optional[dict[str, Meter]] = None
+config: str = "modbus_registers.yaml"
 
 
-def load_register_reference():
-    with open("modbus_registers.yaml", "r") as stream:
-        try:
-            global data, electric
-            data = yaml.safe_load(stream)
-            electric = data["electric_meter"]
-        except yaml.YAMLError as ex:
-            print(ex)
+def _get_meters() -> dict[str, Meter]:
+    global meters
+    if meters is None:
+        _load_register_reference()
+    return meters
+
+
+def _get_electric() -> Meter:
+    meters = _get_meters()
+    return meters["electric"]
+
+
+def _get_panel() -> Meter:
+    return _get_meters()["water_panel"]
+
+
+async def _connect_meter(meter: Meter):
+    if meter.client is None:
+        meter.client = mbc.AsyncModbusTcpClient(meter.id.ip_address, meter.id.tcp_socket)
+    if not meter.client.connected:
+        await meter.client.connect()
+
+
+def _load_register_reference():
+    global config, meters
+    with open(config, "r") as stream:
+        meters = yaml.safe_load(stream)
 
 
 async def _modbus_test(phase: int):
-    global electric
+    global meters
+    electric = meters["electric"]
     assert electric is not None
     client = mbc.AsyncModbusTcpClient(electric.id.ip_address, electric.id.tcp_socket)
     await client.connect()
-    response = await client.read_input_registers(electric.registers["phases"][phase]["voltage"].register, 1, electric.id.slave_id)
+    response = await client.read_input_registers(electric.registers["phases"][phase]["voltage"].register, 1,
+                                                 electric.id.slave_id)
     print(response)
 
     await client.close()
 
 
-async def read(target) -> dict:
-    pass
+def _decode_type(register: Register, decoder: mbp.BinaryPayloadDecoder) -> any:
+    match register.type:
+        case "float":
+            return decoder.decode_32bit_float()
+        case "int":
+            return decoder.decode_16bit_int()
+        case _:
+            raise ValueError(f"Register type unsupported by the decoder: {register.type}")
 
 
-async def _read_register(client, register: Register):
-    response = await client.read_input_registers(register.register, 1, electric.id.slave_id)
-    decoder = mbp.BinaryPayloadDecoder.fromRegisters(response.registers)
-    if register.type == "float":
-        return decoder.decode_32bit_float()
+async def _read_register(meter: Meter, register: Register) -> any:
+    assert meter.client is not None
+    reg_type = meter.register_types[register.type]
+    if reg_type is None:
+        raise ValueError(f"Register type '{register.type}' not found in configuration")
+
+    response = await meter.client.read_input_registers(register.register, reg_type.length, meter.id.slave_id)
+    if response.isError():
+        raise ConnectionError(f"Error reading register {register.register}: {response}")
+
+    decoder = mbp.BinaryPayloadDecoder.fromRegisters(response.registers, reg_type.byteorder, reg_type.wordorder)
+
+    return _decode_type(register, decoder)
 
 
-import asyncio
+async def _read_registers(meter: Meter, registers: dict[str, Register]) -> dict:
+    assert isinstance(registers, dict)
+    await _connect_meter(meter)
+    results = {}
+    for key, register in registers.items():
+        results[key] = await _read_register(meter, register)
+    return results
 
-if __name__ == "__main__":
-    load_register_reference()
-    #assert electric_meter is not None
+
+# Public functions for reading data from specific register sets
+async def read_phases() -> list[dict]:
+    electric = _get_electric()
+    phases = []
+    for phase_id, phase in electric.registers["phases"].items():
+        phases.append(await _read_registers(electric, phase))
+    return phases
+
+
+async def read_avg() -> dict:
+    electric = _get_electric()
+    return await _read_registers(electric, electric.registers["average"])
+
+
+async def read_panel() -> dict:
+    panel = _get_panel()
+    return await _read_registers(panel, panel.registers["panel"])
+
+
+# For testing purposes
+def _test_main():
+    import asyncio
+
+    electric = _get_electric()
     print(electric)
     print(f'voltage register = {electric.registers["phases"][1]["voltage"].register}')
 
-    asyncio.run(_modbus_test(1))
+    res = asyncio.run(read_phases())
+    print("Phases:")
+    print(res)
+
+
+if __name__ == "__main__":
+    meters = None
+    _test_main()
