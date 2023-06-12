b7fe0f51cae9decb93207c0283610f45315dc9bf
Mik3l24 <michalczus@gmail.com>
2023-06-12 21:39:52 +0200

    Added documentation for the scheduler

--- readings/scheduler.py
+++ readings/scheduler.py
@@ -1,3 +1,13 @@
+"""Module for scheduling of taking readings.
+
+It can be run as a standalone script, which launches jobs for all implemented meters,
+or imported and used in another script.
+
+The working directory must be the root of the project (one folder up) for the script to work.
+
+If not specified, the default interval is 15 minutes.
+
+"""
 import threading
 import time
 from datetime import timedelta
@@ -9,6 +19,21 @@ from readings.reading_execution import measure_and_save_avg, measure_and_save_pa
 
 # Base source: https://medium.com/greedygame-engineering/an-elegant-way-to-run-periodic-tasks-in-python-61b7c477b679
 class Job(threading.Thread):
+    """A job that runs periodically.
+
+    Attributes
+    ----------
+    interval : timedelta
+        The interval between executions.
+    execute : function
+        The function to execute periodically.
+    args : list
+        The arguments to pass to the function.
+    kwargs : dict
+        The keyword arguments to pass to the function.
+
+
+    """
     def __init__(self, interval, execute, *args, **kwargs):
         threading.Thread.__init__(self)
         self.daemon = False
@@ -23,6 +48,11 @@ class Job(threading.Thread):
         self.join()
 
     def run(self):
+        """Starts the job.
+
+        Function in field ``execute`` will now be executed periodically, according to the interval.
+
+        """
         while not self.stopped.wait(self.interval.total_seconds()):
             self.execute(*self.args, **self.kwargs)
 
@@ -35,6 +65,21 @@ jobs: dict[str, Optional[Job]] = {
 
 
 def init_jobs():
+    """Initializes the jobs with the intervals specified in the config file.
+
+    Intervals can be configured in the /config/config.ini file: ::
+
+        [reading_intervals]
+        job_name = # Interval in seconds
+
+    Defaults to 15 minutes if an interval is not specified.
+
+    The jobs are:
+        - ``"phases"``: ``measure_and_save_phases``
+        - ``"avg"``: ``measure_and_save_avg``
+        - ``"panel"``: ``measure_and_save_panel``
+
+    """
     try:
         intervals = load_config(section="reading_intervals")
     except ConfigNotFound:
@@ -47,6 +92,11 @@ def init_jobs():
 
 
 def start_jobs():
+    """Starts all the jobs.
+
+    If the jobs have not been initialized, ``init_jobs()`` is run.
+
+    """
     if any(job is None for job in jobs.values()):
         init_jobs()
     for job in jobs.values():
